e4301bfe142869f852bf48b622fbe6154154697e
fks213 <fks213@mail.ru>
2019-01-11 00:55:21 +0700

    Add method to show different sprites

--- app/sprite.rb
+++ app/sprite.rb
@@ -1,5 +1,25 @@
 # class Sprite
 class MySprite
   def initialize
+    @x = 181
+    @y = 225
+    @clip_width = 150
+    @time = 200
+    @loop = true
+    @sprite = Square.new
+    @music = Music.new('../sound/click.mp3')
+  end
+
+  def invalidate(creature)
+    @sprite.remove
+    @sprite = Sprite.new(
+      "../images/sprites/#{creature}.png",
+      x: @x,
+      y: @y, z: 10,
+      clip_width: @clip_width,
+      time: @time,
+      loop: @loop
+    )
+    @sprite.play
   end
 end

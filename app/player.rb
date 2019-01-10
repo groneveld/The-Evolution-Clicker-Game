5b87673b4efad8a965707d0b5fa192833f26aacc
groneveld <groneveld.co@gmail.com>
2019-01-10 23:25:52 +0700

    Levels_up params & it's info view added

--- app/player.rb
+++ app/player.rb
@@ -10,6 +10,25 @@ class Player
     @evolution_level = 1
     @factor = 1
     @dna_required = @evolution_level * 10 * @factor
+    @require_text = Text.new('')
+  end
+
+  def draw_require
+    @require_text.remove
+    @require_text = Text.new(
+      'Required: ' + @dna_required.to_s + ' DNA',
+      x: 705, y: 155,
+      size: 17,
+      color: '#FFFFE0',
+      z: 10
+    )
+  end
+
+  def levels_up
+    @evolution_level += 1
+    @factor += @evolution_level
+    @summary_dna -= @dna_required
+    @dna_required = @evolution_level * 10 * @factor
   end
 
 end

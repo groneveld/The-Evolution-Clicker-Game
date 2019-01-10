c9d1b308a115232372ecc0074c358ea0b7d74fe8
fks213 <fks213@mail.ru>
2019-01-10 23:55:22 +0700

    Get saved player parametrs

--- app/bd.rb
+++ app/bd.rb
@@ -2,6 +2,11 @@ require 'pg'
 
 # class DataBase
 class DataBase
+  attr_accessor :creatures,
+                :level,
+                :dna,
+                :factor
+
   def initialize
     @creatures_conn = PG.connect(dbname: 'evolution')
     @creatures = {}
@@ -13,6 +18,16 @@ class DataBase
     @creatures_conn.exec('SELECT * FROM creatures') do |result|
       result.each do |row|
         @creatures[row['level'].to_i] = row['name'].strip
+      end
+    end
+  end
+
+  def player_get(name)
+    @players_conn.exec("SELECT * FROM users WHERE name='#{name}'") do |result|
+      unless result.values.empty?
+        @level = result.values[0][1].to_i
+        @dna = result.values[0][2].to_i
+        @factor = result.values[0][3].to_i
       end
     end
   end

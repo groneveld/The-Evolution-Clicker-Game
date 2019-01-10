ec9de50354a744aa50454e194647d512f3a9a917
fks213 <fks213@mail.ru>
2019-01-11 00:37:55 +0700

    Add method for sprite show

--- app/bonus.rb
+++ app/bonus.rb
@@ -1,5 +1,21 @@
 # class Bonus
 class Bonus
   def initialize
+    @sprite = Square.new(z: -1)
+    @clip_width = 150
+    @x = 0
+    @y = 0
+    @status = 'good'
+  end
+
+  def draw
+    @x = rand(350)
+    @y = rand(450)
+    @sprite = Sprite.new(
+      '../images/' + @status + '-bonus.png',
+      x: @x, y: @y, z: 100,
+      clip_width: @clip_width, time: 100, loop: true
+    )
+    @sprite.play
   end
 end

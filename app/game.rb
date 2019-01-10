24e21a0a8f318cc8e6c6e9fb1f5be562d99ba219
fks213 <fks213@mail.ru>
2019-01-11 01:32:41 +0700

    Add objects initialization

--- app/game.rb
+++ app/game.rb
@@ -1,5 +1,20 @@
 # class Game
 class Game
+  attr_accessor :player,
+                :sprite,
+                :creature,
+                :bonus
+
   def initialize
+    @player = Player.new(name)
+    @sprite = MySprite.new
+    sprite_invalidate
+    @creature = Creature.new(1000)
+    @bonus = Bonus.new
+    @bonus.init_bonus
+  end
+
+  def sprite_invalidate
+    @sprite.invalidate(@player.db.creatures[@player.evolution_level])
   end
 end

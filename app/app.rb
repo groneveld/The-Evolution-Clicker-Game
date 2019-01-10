17ce5fd592411d08ec3315b3f0d50627af4de3cc
groneveld <groneveld.co@gmail.com>
2019-01-10 23:01:29 +0700

    fixed file structure

--- app/app.rb
+++ app/app.rb
@@ -3,3 +3,15 @@ require 'ruby2d'
 require_relative './window'
 
 window = MyWindow.new
+
+on :key_down do |event|
+  if window.is_enter
+    window.username += event.key if event.key !~ /\w./
+    window.username = window.username.chop if event.key == 'backspace'
+  end
+  if event.key == 'return'
+    #game = Game.new(window.username)
+    window.is_enter = false
+    window.init_game
+  end
+end

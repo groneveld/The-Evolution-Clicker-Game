bc78435febf0936f88b4c392780d20141ff91ea8
fks213 <fks213@mail.ru>
2019-01-10 23:52:36 +0700

    Get list of creatures

--- app/bd.rb
+++ app/bd.rb
@@ -5,6 +5,15 @@ class DataBase
   def initialize
     @creatures_conn = PG.connect(dbname: 'evolution')
     @creatures = {}
+    creatures_get
     @players_conn = PG.connect(dbname: 'tecg_users')
+  end
+
+  def creatures_get
+    @creatures_conn.exec('SELECT * FROM creatures') do |result|
+      result.each do |row|
+        @creatures[row['level'].to_i] = row['name'].strip
+      end
+    end
   end
 end
